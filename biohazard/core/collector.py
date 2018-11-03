97f50a048f5051153514d4862430a10e19ddfbaa
Yennick Schepers <yennicks@users.noreply.github.com>
2018-11-03 22:54:26 +0100

    Refactor collecting and use more parameters derived from settings.py

--- biohazard/core/collector.py
+++ biohazard/core/collector.py
@@ -1,13 +1,14 @@
-import pathlib
 import shutil
 
-from biohazard.conf.settings import output_path
+from biohazard.conf.settings import assets_path, contents_path, output_path
 from biohazard.core.io import get_contents, load_yaml
 from biohazard.core.render import render
 
 
 def collect_assets():
-    assets_path = pathlib.Path('assets')
+    """
+    Collect all static assets from the assets directory if it exists.
+    """
     if not assets_path.exists():
         raise Exception("Path does not exist.")
 
@@ -15,17 +16,14 @@ def collect_assets():
 
 
 def collect_contents():
-    contents_dir = pathlib.Path('contents')
-    files = get_contents(contents_dir)
+    """
+    Render all content and collect it.
+    """
+    files = get_contents(contents_path)
+
     for file in files:
         data = load_yaml(file)
         rendered_data = render(data)
         workfile = file.stem + '.html'
         with open(output_path / workfile, 'w') as stream:
             stream.write(rendered_data)
-
-    # gitignore
-    # ready for v0.0
-    # upload to gitlab
-    # register domain
-    # v0.1 proper testing, documentation
