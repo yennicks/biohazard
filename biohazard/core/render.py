7640c404e22d50f437c04001aa496bdc317706ad
Yennick Schepers <yennicks@users.noreply.github.com>
2018-11-03 22:55:48 +0100

    Inline documentation of renderer.

--- biohazard/core/render.py
+++ biohazard/core/render.py
@@ -1,7 +1,13 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, PackageLoader
 
 
 def render(data: dict) -> str:
+    """
+    Run the jinja2 render according to the parameters defined in the data dictionary.
+
+    :param data: Any complaint biohazard data dictionary.
+    :return: string of rendered data
+    """
     template_name = data.get('template', 'bio')
     content = data['content']
 
